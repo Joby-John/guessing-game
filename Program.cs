6940e9cc2b3ff0635edd2cea8426190d29074894
Joby John <joby432john@gmail.com>
2022-08-12 11:35:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to guessing game");
+Console.WriteLine("question master's time , enter the word which you want others to guess");
+string qn = Console.ReadLine().ToLower();
+string guess = "";
+Console.Clear();
+Console.WriteLine("Now its the time to guess");
+
+do
+{
+    Console.WriteLine("Enter your guess");
+    Console.WriteLine("Enter : I give up, to give up");
+    guess = Console.ReadLine().ToLower();
+    if (guess == "i give up")
+    {
+        Console.WriteLine("Oops sorry, you tried hard, it was :{0} ", qn);
+        break;
+    }
+
+} while (guess != qn);
+    if (guess == qn)
+{
+    Console.WriteLine("\nYou guessed it right,HURRAY!");
+}
